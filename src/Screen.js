f2a811f03191be8971666491ea5f97dedebf1175
34selen <73600142+34selen@users.noreply.github.com>
2023-08-11 20:47:17 +0900

    f

--- src/Screen.js
+++ src/Screen.js
@@ -1,8 +1,23 @@
 import "./Screen.css"
+import Select from "./Select";
 function Screen(){
 
     return(
-        <div id="screen"></div>
+        <div id="screen">
+            <div id="top-bar"></div>
+            <div id="middle-bar">
+                <div id="mySelect">
+                    <h1>You</h1>
+                </div>
+                <div id="divided-line"></div>
+                <div id="comSelect">
+                    <h1>Computer</h1>
+                </div>
+            </div>
+            <div id="botton-bar">
+                <Select />
+            </div>
+        </div>
     )
 }
 
---
+++ src/Select.js
@@ -0,0 +1,18 @@
+function Select(){
+    return(
+        <div>
+            <div id="rock">
+                <img src="./img/rock.jpeg"></img>
+            </div>
+            <div id="sissor">
+            <img src="./img/sissor.jpeg"></img>
+            </div>
+            <div id="paper">
+            <img src="./img/paper.jpeg"></img>
+            </div>
+        </div>
+    )
+    
+    
+}
+export default Select
