b5451f63b80a6895e7687136794e1a18619e4ac5
34selen <73600142+34selen@users.noreply.github.com>
2023-08-12 14:38:56 +0900

    j

--- src/Select.js
+++ src/Select.js
@@ -2,13 +2,13 @@ function Select(){
     return(
         <div>
             <div id="rock">
-                <img src="./img/rock.jpeg"></img>
+                <img src="./img/rock.jpeg" alt="Rock" />
             </div>
             <div id="sissor">
-            <img src="./img/sissor.jpeg"></img>
+                <img src="./img/sissor.jpeg"alt="Sissor"/>
             </div>
             <div id="paper">
-            <img src="./img/paper.jpeg"></img>
+                <img src="./img/paper.jpeg" alt="paper" />
             </div>
         </div>
     )
