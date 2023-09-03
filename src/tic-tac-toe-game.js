8123f81c96e04b4fe96b77c15ad9f4da5e7bf38a
34selen <73600142+34selen@users.noreply.github.com>
2023-09-03 16:46:00 +0900

    클릭하면 동그라미 생김

--- src/tic-tac-toe-game.js
+++ src/tic-tac-toe-game.js
@@ -1,22 +1,65 @@
 import styles from "./tic-tac-toe-game.module.css"
+import React, { useState } from 'react';
 function Tic_Tac_Toe_game(){
+    const [imageState,setimageState]=useState({
+        one:null,
+        two:null,
+        three:null,
+        four:null,
+        five:null,
+        six:null,
+        seven:null,
+        eight:null,
+        nine:null
+    })
+    function handleClick(event) {
+        // event.target.className 은 클릭된 div의 모든 클래스 이름을 반환합니다.
+        // split을 사용하여 첫 번째 클래스 이름만 가져옵니다.
+        const firstClass = event.target.className.split(" ")[0];
+        
+        const newImageState = {
+            ...imageState,
+            [firstClass]: './img/tic-tac-toe/O_mark.png',
+        };
+        setimageState(newImageState);
+        console.log(firstClass);
+        console.log(imageState);
+    }
     return(
         <div className={styles.screen}>
             <div className={styles.areaContainer}>
                 <div className={styles.line}>
-                    <div className={styles.one} className={styles.area}></div>
-                    <div className={styles.two} className={styles.area}></div>
-                    <div className={styles.three} className={styles.area}></div>
+                    <div className={`one ${styles.area}`} onClick={handleClick}>
+                        {imageState.one && <img className={styles.O_Ximg}src={imageState.one} alt="icon" />}
+                    </div>
+                    <div className={`two ${styles.area}`} onClick={handleClick}>
+                        {imageState.two && <img className={styles.O_Ximg} src={imageState.two} alt="icon" />}
+                    </div>
+                    <div className={`three ${styles.area}`} onClick={handleClick}>
+                        {imageState.three && <img className={styles.O_Ximg} src={imageState.three} alt="icon" />}
+                    </div>
                 </div>
                 <div className={styles.line}>
-                    <div className={styles.four} className={styles.area}></div>
-                    <div className={styles.five} className={styles.area}></div>
-                    <div className={styles.six} className={styles.area}></div>
+                    <div className={`four ${styles.area}`} onClick={handleClick}>
+                        {imageState.four && <img className={styles.O_Ximg}src={imageState.four} alt="icon" />}
+                    </div>
+                    <div className={`five ${styles.area}`} onClick={handleClick}>
+                        {imageState.five && <img className={styles.O_Ximg}src={imageState.five} alt="icon" />}
+                    </div>
+                    <div className={`six ${styles.area}`} onClick={handleClick}>
+                        {imageState.six && <img className={styles.O_Ximg}src={imageState.six} alt="icon" />}
+                    </div>
                 </div>
                 <div className={styles.line}>
-                    <div className={styles.seven} className={styles.area}></div>
-                    <div className={styles.eight} className={styles.area}></div>
-                    <div className={styles.nine} className={styles.area}></div>
+                    <div className={`seven ${styles.area}`} onClick={handleClick}>
+                        {imageState.seven && <img className={styles.O_Ximg}src={imageState.seven} alt="icon" />}
+                    </div>
+                    <div className={`eight ${styles.area}`} onClick={handleClick}>
+                        {imageState.eight && <img className={styles.O_Ximg}src={imageState.eight} alt="icon" />}
+                    </div>
+                    <div className={`nine ${styles.area}`} onClick={handleClick}>
+                        {imageState.nine && <img className={styles.O_Ximg}src={imageState.nine} alt="icon" />}
+                    </div>
                 </div>
             </div>
         </div>
